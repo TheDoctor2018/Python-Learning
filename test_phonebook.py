be591e66b0f4a3eaa3e8d3dfc9ac65f811e0d50d
TheDoctor2018 <carlosivasquez2018@gmail.com>
2022-04-05 19:25:47 -0500

    Test Commit

--- test_phonebook.py
+++ test_phonebook.py
@@ -1,15 +1,16 @@
 import unittest
 
+from phonebook import PhoneBook
+
 
 class PhoneBookTest(unittest.TestCase):
     def test_lookup_by_name(self):
-        phonebook = phonebook()
+        phonebook = PhoneBook()
         phonebook.add("Bob", "12345")
         number = phonebook.lookup("Bob")
         self.assertEqual("12345", number)
 #Sample for testing
-    def test_anothertest(self):
-        self.assertEqual(3,4)
+#        self.assertEqual(3,4)
 
 # Main needs to be added for unitest pyhon -m unittest to pick up the test file.
 # install the correct extension Python from Microsoft
