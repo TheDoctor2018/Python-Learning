2734995640edbc474aad53478d357f41deb83563
TheDoctor2018 <carlosivasquez2018@gmail.com>
2022-04-05 19:05:21 -0500

    Sample for bad unit test, for def its always test_

--- test_phonebook.py
+++ test_phonebook.py
@@ -2,12 +2,14 @@ import unittest
 
 
 class PhoneBookTest(unittest.TestCase):
-    def Test_lookup_by_name(self):
+    def test_lookup_by_name(self):
         phonebook = phonebook()
         phonebook.add("Bob", "12345")
         number = phonebook.lookup("Bob")
         self.assertEqual("12345", number)
-
+#Sample for testing
+    def test_anothertest(self):
+        self.assertEqual(3,4)
 
 # Main needs to be added for unitest pyhon -m unittest to pick up the test file.
 # install the correct extension Python from Microsoft
